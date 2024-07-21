c90993ad07a1d4f48328551ba944641243add59d
Nicolasjr0010 <nicolasr232330@gmail.com>
2024-07-20 23:18:39 -0600

    Templates modificados para mostrar post desde base de datos.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-92l@=l&rez0w8-as@y&9-kk3(j)m%&h6kg59g=g8*aqdr=vs82'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
+DEBUG = False  
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
